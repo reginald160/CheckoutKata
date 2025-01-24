523e25b96733cdf96f0711b87853c2614394885a
Ifeanyi <ozougwuifeanyi160@gmail.com>
2025-01-24 20:43:49 +0000

    Add unit tests for total price calculation and pricing rules to define TDD structure

---
+++ CheckoutKata/Checkout.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutKata
+{
+    // Placeholder class to satisfy the compiler; these will be implemented later as part of TDD
+    public class Checkout
+    {
+        public void Scan(string item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetTotalPrice()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CheckoutKata/Test/CheckoutTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CheckoutKata.Test
+{
+    public class CheckoutTest
+    {
+        [Fact]
+        public void TestTotals()
+        {
+   
+            var checkout = new Checkout(); 
+
+
+            Assert.Equal(0, checkout.GetTotalPrice()); 
+
+            checkout.Scan("A");
+            Assert.Equal(50, checkout.GetTotalPrice());
+
+            checkout.Scan("B");
+            Assert.Equal(80, checkout.GetTotalPrice());
+
+            checkout.Scan("A");
+            Assert.Equal(130, checkout.GetTotalPrice());
+
+            checkout.Scan("A");
+            Assert.Equal(160, checkout.GetTotalPrice());
+
+            checkout.Scan("B");
+            Assert.Equal(175, checkout.GetTotalPrice());
+        }
+ 
+
+        [Fact]
+        public void TestNoPricingRules()
+        {
+
+            var checkout = new Checkout(); 
+
+            Assert.Throws<ArgumentException>(() => checkout.Scan("A")); 
+        }
+
+        [Fact]
+        public void TestInvalidItem()
+        {
+           
+            var checkout = new Checkout();
+
+            Assert.Throws<ArgumentException>(() => checkout.Scan("X")); 
+        }
+    }
+}
+
